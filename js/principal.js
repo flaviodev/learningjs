e766c1988df0f34d158aedb7b7601f653d9e06f6
flavio <fdsdev@gmail.com>
2018-08-04 18:19:03 -0300

    finalizando aula 2

--- js/principal.js
+++ js/principal.js
@@ -1,18 +1,28 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var trPaciente = document.querySelector('#primeiro-paciente');
+var tdPeso = trPaciente.querySelector('.info-peso');
+var tdAltura = trPaciente.querySelector('.info-altura');
+var tdImc = trPaciente.querySelector('.info-imc');
 
-var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
-
-var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-var imc = peso / (altura * altura);
+var pesoEhValido = true; 
+var alturaEhValida = true; 
 
-var tdImc = paciente.querySelector(".info-imc");
-
-var imc = peso / (altura * altura);
-
-tdImc.textContent = imc;
+if(peso <=0 || peso >= 1000) {
+    pesoEhValido = false;
+    tdPeso.textContent = 'peso inválido';
+ }
+ 
+ if(altura <= 0 || altura >= 3.00) {
+   alturaEhValida = false;
+   tdAltura.textContent = 'altura inválida';
+ }
+ 
+ if(pesoEhValido && alturaEhValida) {
+    var imc = peso / ( altura * altura);
+    tdImc.textContent = imc; 
+ }
