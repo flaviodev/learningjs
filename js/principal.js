6d4a3230cdca19106fb5338ba53dab4a5ccf0848
flavio <fdsdev@gmail.com>
2018-08-05 17:56:58 -0300

    implementando formulario da aula 4 com recursos a mais

--- js/principal.js
+++ js/principal.js
@@ -4,12 +4,79 @@ titulo.textContent = "Aparecida Nutricionista";
 var pacientes = document.querySelectorAll('.paciente');
 
 for(var i = 0; i < pacientes.length; i++) {
+  populaPaciente(pacientes[i]);
+}
 
-  var paciente = pacientes[i];
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+  event.preventDefault();
+  var form = document.querySelector("#form-adiciona");
 
-  var tdPeso = paciente.querySelector('.info-peso');
-  var tdAltura = paciente.querySelector('.info-altura');
-  var tdImc = paciente.querySelector('.info-imc');
+  var nome = form.nome.value;
+  var peso = form.peso.value;
+  var altura = form.altura.value;
+  var gordura = form.gordura.value;
+
+  var pacienteTr = document.createElement("tr");
+  pacienteTr.classList.add("paciente");
+
+  var nomeTd = document.createElement("td");
+  nomeTd.classList.add("info-nome");
+  var pesoTd = document.createElement("td");
+  pesoTd.classList.add("info-peso");
+  var alturaTd = document.createElement("td");
+  alturaTd.classList.add("info-altura");
+  var gorduraTd = document.createElement("td");
+  gorduraTd.classList.add("info-gordura");
+  var imcTd = document.createElement("td");
+  imcTd.classList.add("info-imc");
+
+  nomeTd.textContent = nome;
+  pesoTd.textContent = peso;
+  alturaTd.textContent = altura;
+  gorduraTd.textContent = gordura;
+
+  pacienteTr.appendChild(nomeTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(gorduraTd);
+  pacienteTr.appendChild(imcTd);
+
+
+  var tabela = document.querySelector("#tabela-pacientes");
+
+  tabela.appendChild(pacienteTr);
+
+  populaPaciente(pacienteTr);
+
+  form.nome.value = '';
+  form.peso.value = '';
+  form.altura.value = '';
+  form.gordura.value = '';
+});
+
+function populaPaciente(trPaciente) {
+  var tdPeso = trPaciente.querySelector('.info-peso');
+  var tdAltura = trPaciente.querySelector('.info-altura');
+  var tdImc = trPaciente.querySelector('.info-imc');
+
+  var peso = tdPeso.textContent;
+  var altura = tdAltura.textContent;
+  
+  if(isPesoEAlturaValidos(trPaciente)) {
+    tdImc.textContent = calculaImc(peso,altura).toFixed(2);
+  }
+}
+
+
+function calculaImc(peso, altura) {
+  return peso / ( altura * altura);
+}
+
+function isPesoEAlturaValidos(trPaciente) {
+
+  var tdPeso = trPaciente.querySelector('.info-peso');
+  var tdAltura = trPaciente.querySelector('.info-altura');
 
   var peso = tdPeso.textContent;
   var altura = tdAltura.textContent;
@@ -20,17 +87,14 @@ for(var i = 0; i < pacientes.length; i++) {
   if(peso <=0 || peso >= 200) {
     pesoEhValido = false;
     tdPeso.textContent =  tdPeso.textContent + ' (peso inválido)';
-    paciente.classList.add("paciente-com-valor-invalido");
+    trPaciente.classList.add("paciente-com-valor-invalido");
   }
   
   if(altura <= 0 || altura >= 3.00) {
     alturaEhValida = false;
     tdAltura.textContent = tdAltura.textContent + ' (altura inválida)';
-    paciente.classList.add("paciente-com-valor-invalido");
+    trPaciente.classList.add("paciente-com-valor-invalido");
   }
   
-  if(pesoEhValido && alturaEhValida) {
-    var imc = peso / ( altura * altura);
-    tdImc.textContent = imc.toFixed(2); 
-  }
+  return pesoEhValido && alturaEhValida;
 }
